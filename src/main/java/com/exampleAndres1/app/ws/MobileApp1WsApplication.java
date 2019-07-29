79e10b2ca9ff8729b545108e45cb7e08c846cd82
AndresHMosqueda <andreshm94@gmail.com>
2019-07-28 19:49:20 -0500

    first commit

---
+++ src/main/java/com/exampleAndres1/app/ws/MobileApp1WsApplication.java
@@ -0,0 +1,13 @@
+package com.exampleAndres1.app.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileApp1WsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileApp1WsApplication.class, args);
+	}
+
+}
